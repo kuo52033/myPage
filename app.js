379b7f5ad8ce657a794a3bc0387a293eb48d3496
Tim Kuo <tim31422@gmail.com>
2021-09-30 16:42:37 +0800

    把長度都修改成rem，開始使用bem命名法

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const navbar = document.querySelector(".navbar");
+const navbar = document.querySelector(".header__nav");
 
 window.addEventListener("scroll", () => {
   navbar.classList.toggle("sticky", this.scrollY > navbar.clientHeight);
