87d4853f03e66725816c1b3261d21ae858efb4a1
Tim Kuo <tim31422@gmail.com>
2021-10-01 17:14:07 +0800

    修改header

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const navbar = document.querySelector(".header__nav");
+// const navbar = document.querySelector(".header__nav");
 
-window.addEventListener("scroll", () => {
-  navbar.classList.toggle("sticky", this.scrollY > navbar.clientHeight);
-});
+// window.addEventListener("scroll", () => {
+//   navbar.classList.toggle("sticky", this.scrollY > navbar.clientHeight);
+// });
